c25a8515768954f314c5d8a3aaa26c8c6be94bc6
63n0m3 <88727337+63n0m3@users.noreply.github.com>
2021-11-24 16:22:20 +0100

    Add files via upload

--- dependencies/Extension_to_Adafruit1p0.hpp
+++ dependencies/Extension_to_Adafruit1p0.hpp
@@ -3,8 +3,8 @@ class Custom_Button{  /// This class is an extension to functionality of Adafrui
     int16_t Top_Left_X;     /// as arrays of 16 bit (RGB 5/6/5) with element number of width*height
     int16_t Top_Left_Y;     /// Alternatively it can also be a monochrome bitmap of uint8_t with true/false colors defined in initButton function
     const uint16_t* Bitmap_Progmem_Pos;     /// You can recall initButton function multiple times during execution of a program
-    const uint8_t* Mask_Progmem_Pos;        /// This comes with example of usage and video tutorial.
-    const uint16_t* Bitmap_Progmem_Neg;     /// by Gen0me. Free to use. github.com/63n0m3/Custom_Button/ btc: 1A8Gqs2JwdjuAfFZ4cnM4CWy9hGFzsCpzK
+    const uint8_t* Mask_Progmem_Pos;        /// This comes with example of usage, short documentation and video tutorial.
+    const uint16_t* Bitmap_Progmem_Neg;     /// by Gen0me. MIT licence. github.com/63n0m3/Custom_Button/ btc: 1A8Gqs2JwdjuAfFZ4cnM4CWy9hGFzsCpzK
     const uint8_t* Mask_Progmem_Neg;
     const uint8_t* Mon_Bitmap_Progmem;
     uint16_t Color_True;
@@ -13,14 +13,14 @@ class Custom_Button{  /// This class is an extension to functionality of Adafrui
     bool currstate, laststate;
 
   public:
-    unsigned long Last_Time_Used_millis;    /// You can use this varieable to time multiple pressings on one hold
+    unsigned long Last_Time_Used_millis;    /// You can use this varieable to time multiple pressings on one hold. The current time is always being save to it with every draw call.
     int16_t Width;
     int16_t Height;
 
     void initButton(int16_t top_left_x, int16_t top_left_y, const uint16_t bitmap_progmem_pos[], const uint8_t mask_progmem_pos[],
     const uint16_t bitmap_progmem_neg[], const uint8_t mask_progmem_neg[], int16_t width, int16_t height, Adafruit_GFX* display){
-      Top_Left_X = top_left_x - (width/2);                          /// mask controls transparency, if mask_progmem_pointer points to NULL, there is no
-      Top_Left_Y = top_left_y - (height/2);                          /// transarency and different drawing function is used (possibly faster)
+      Top_Left_X = top_left_x - (width/2);                          /// mask controls transparency, if mask_progmem_pointers point to NULL, there is no
+      Top_Left_Y = top_left_y - (height/2);                          /// transarency and different drawing function is used
       Bitmap_Progmem_Pos = bitmap_progmem_pos;          /// 16-bit image (RGB 5/6/5) with a 1-bit mask (set bits = opaque, unset bits = clear)
       Mask_Progmem_Pos = mask_progmem_pos;
       Bitmap_Progmem_Neg = bitmap_progmem_neg;
@@ -33,7 +33,7 @@ class Custom_Button{  /// This class is an extension to functionality of Adafrui
       laststate = false;
       Last_Time_Used_millis = millis();
     }
-                                                                  ///For 1 bit per pixel(monochrome bitmaps)
+                                                                  ///For 1 bit per pixel(monochrome bitmap acting as mask for drawing pre-defined rgb 565 color)
     void initButton(int16_t top_left_x, int16_t top_left_y, const uint8_t mon_bitmap_progmem[], uint16_t color_true, uint16_t color_false, int16_t width, int16_t height, MCUFRIEND_kbv* display){
       Top_Left_X = top_left_x - (width/2);
       Top_Left_Y = top_left_y - (height/2);
@@ -51,8 +51,8 @@ class Custom_Button{  /// This class is an extension to functionality of Adafrui
       laststate = false;
       Last_Time_Used_millis = millis();
     }
-    void drawButton(boolean used){
-      Last_Time_Used_millis = millis();
+    void drawButton(boolean used){      /// Function that actually draws the button
+      Last_Time_Used_millis = millis(); /// Depending on initButton function earlier it will choose different functions to draw button
       if (used == true){
         if (Mon_Bitmap_Progmem !=NULL){
           Display->drawBitmap(Top_Left_X, Top_Left_Y, Mon_Bitmap_Progmem, Width, Height, Color_True);
@@ -74,21 +74,21 @@ class Custom_Button{  /// This class is an extension to functionality of Adafrui
         }
       }
     }
-    bool contains(int16_t point_x, int16_t point_y){
+    bool contains(int16_t point_x, int16_t point_y){    /// Function that checks if X and Y coordinates are inside the button or not
       if ((point_x > Top_Left_X && point_x < Top_Left_X + Width) && point_y > Top_Left_Y && point_y < Top_Left_Y + Height) return true;
       else return false;
     }
-    void press(bool p){
-      laststate = currstate;
+    void press(bool p){         /// this is the refresh function while using with Adafruits approach, should be called every main loop frame.
+      laststate = currstate;    /// the argument inside is true/false condition that should determine if the is pressed down or not on current frame using the contains function and cursor
       currstate = p;
     }
-    bool justPressed(){
+    bool justPressed(){         /// if the button was just pressed using usuall Adafruit approach with press() refresh on every main loop frame
       return (currstate && !laststate);
     }
-    bool justReleased(){
+    bool justReleased(){        /// if the button was just released using usuall Adafruit approach with press() refresh on every main loop frame
       return (!currstate && laststate);
     }
-    void Change_Position(int16_t mid_x, int16_t mid_y){
+    void Change_Position(int16_t mid_x, int16_t mid_y){     /// Function to change buttons drawing position for future draw calls, instead of making the long call to initButton(..)
       Top_Left_X = mid_x - (Width/2);
       Top_Left_Y = mid_y - (Height/2);
     }
